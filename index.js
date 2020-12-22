8a672c97f8b47257c1144ed63754d6b8f08d5b94
Craig Smith <craigjsmith99@gmail.com>
2020-12-22 00:01:34 -0500

    Update index.js

--- index.js
+++ index.js
@@ -20,12 +20,6 @@ var api = new ParseServer({
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   }
-  'filesAdapter': {
-    'module': 'parse-server-s3-adapter',
-    'options': {
-      'bucket': "proxima-storage",
-    }
-  }
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
